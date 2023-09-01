0a61a5e99b22bc3f6de736306606ed83e1f7cb0a
Kevi22 <weikevin.fu@gmail.com>
2023-09-01 12:00:44 +0200

    L006

---
+++ Code-Alongs/test.py
@@ -0,0 +1,22 @@
+name = "kevin" # input("Enter your name: ")
+myString = f"hello {name}!"
+print(myString)
+
+print(f"{5 + 8 = }")
+
+print(f"Hej 'Kevin'.")
+print(f'Hej "Kevin".')
+print(f"Hej \'Kevin'.")
+print(f"{3.14666:.2f}")
+print(f"{3000000000:.2e}")
+print(f"{0.5:.2%}")
+print(f"{100000000000:,.2f}")
+
+
+
+first_name = "kevin"
+last_name ="Fu"
+print(f"{'Firstname':20}{'lastname'}")
+print(f"{first_name:x^20}{last_name}")
+print(f"{first_name:x^20}{last_name}")
+

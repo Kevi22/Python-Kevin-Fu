f497b3cd0b849c28ccb1f576925f47100d596e40
Kevi22 <weikevin.fu@gmail.com>
2023-10-17 14:30:59 +0200

    Added Parents class and UML folder

---
+++ Code-Alongs/L027_Module.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range (5):
+        print(f"{i} = {square(i)}")
+
+def square (n):
+    return n * n
+
+if __name__ == "__name__":
+    main()
---
+++ Code-Alongs/L028_Unit_test.py
@@ -0,0 +1,12 @@
+from L027_Module import square
+
+def main():
+    test_square()
+
+
+def test_square():
+    assert (2) == 4 
+
+# print(L027_Module.square(9))
+
+main()
